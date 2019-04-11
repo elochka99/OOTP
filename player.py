2ce2dc2f8849448961d3118bff5eb07859ec4e21
elochka99 <elochka150899@gmail.com>
2019-04-11 22:38:13 +0300

    add some comments

--- player.py
+++ player.py
@@ -3,7 +3,14 @@ from pygame import mixer
 
 
 class Player:
+    """
+    This class can play mp3 track.
+    """
     def __init__(self, file_path=None):
+        """
+        Initialize the class.
+        :param file_path: _path to mp3 file
+        """
         if isinstance(file_path, str):
             if not os.path.isfile(file_path):
                 raise FileNotFoundError('this _path does not conrain usr_input file')
@@ -18,18 +25,30 @@ class Player:
 
     @staticmethod
     def play():
+        """
+        Start playing current file.
+        """
         mixer.music.play()
 
     @staticmethod
     def pause():
+        """
+        Pause current playing track.
+        """
         mixer.music.pause()
 
     @staticmethod
     def unpause():
+        """
+        Continue playing current track, after pause.
+        """
         mixer.music.unpause()
 
     @staticmethod
     def stop():
+        """
+        Stop playing current track.
+        """
         mixer.music.stop()
 
 
