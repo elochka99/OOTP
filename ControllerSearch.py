b0dfbcf08ebb10fdf48556a619c23f86166efcd2
elochka99 <elochka150899@gmail.com>
2019-04-18 00:32:50 +0300

    add comments

--- ControllerSearch.py
+++ ControllerSearch.py
@@ -5,19 +5,32 @@ import webbrowser
 
 @singleton
 class Search(object):
-
+    """
+    Controller search view
+    """
     def __init__(self):
+        """
+        Initialize controller and trigger buttons
+        """
         self.ui = SearchDialog()
         self.ui.trigger(self.search)
 
     def show(self):
-
+        """
+        show user interface
+        """
         self.ui.show()
 
     def hide(self):
+        """
+        hide user interface
+        """
         self.ui.hide()
 
     def search(self):
+        """
+        On submit button search
+        """
         if self.ui.web_url != '':
             webbrowser.open('https://soundcloud.com/search/sounds?q={}'
                             .format(self.ui.web_url))
