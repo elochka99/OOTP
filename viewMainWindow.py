a4dd4f331b98242106038cafeb4c456b32a3ec59
elochka99 <elochka150899@gmail.com>
2019-04-17 22:19:08 +0300

    add comments

--- viewMainWindow.py
+++ viewMainWindow.py
@@ -7,6 +7,9 @@ from PyQt5 import uic
 @singleton
 class MainWindow(QMainWindow):
     def __init__(self):
+        """
+        Initialize the CLASS.
+        """
         super().__init__()
         uic.loadUi('tag_editor.ui', self)
         self.setWindowTitle('Music Tag Viewer')
@@ -22,6 +25,17 @@ class MainWindow(QMainWindow):
     def trigger(self, cell_clicked, show_folder_dialog,
                 show_file_dialog, show_quit_message,
                 show_web_search_dialog, play, pause, stop):
+        """
+        Bind view elements to controller elements
+        :param cell_clicked: action
+        :param show_folder_dialog: action
+        :param show_file_dialog: action
+        :param show_quit_message: action
+        :param show_web_search_dialog: action
+        :param play: action
+        :param pause: action
+        :param stop: action
+        """
 
         self.tracksTable.cellClicked.connect(cell_clicked)
         self.actionAdd_Folder.triggered.connect(show_folder_dialog)
