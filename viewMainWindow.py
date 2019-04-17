4679cbcad72716fe168938448362f7a7dc8694ab
elochka99 <elochka150899@gmail.com>
2019-04-17 22:12:16 +0300

    add def trigger()

--- viewMainWindow.py
+++ viewMainWindow.py
@@ -11,8 +11,23 @@ class MainWindow(QMainWindow):
         uic.loadUi('tag_editor.ui', self)
         self.setWindowTitle('Music Tag Viewer')
         self.setWindowIcon(QIcon('icons/window_icon.png'))
-        self.setWindowFlags(Qt.WindowMinimizeButtonHint | Qt.WindowMaximizeButtonHint | Qt.WindowCloseButtonHint)
+        self.setWindowFlags(Qt.WindowMinimizeButtonHint
+                            | Qt.WindowMaximizeButtonHint
+                            | Qt.WindowCloseButtonHint)
         stylesheet = "::section{Background-color:rgb(204,204,204);}"
         self.tracksTable.horizontalHeader().setStyleSheet(stylesheet)
         self.tagsTable.horizontalHeader().setStyleSheet(stylesheet)
         self.tracksTable.resizeColumnToContents(1)
+
+    def trigger(self, cell_clicked, show_folder_dialog,
+                show_file_dialog, show_quit_message,
+                show_web_search_dialog, play, pause, stop):
+
+        self.tracksTable.cellClicked.connect(cell_clicked)
+        self.actionAdd_Folder.triggered.connect(show_folder_dialog)
+        self.actionAdd_File.triggered.connect(show_file_dialog)
+        self.actionExit.triggered.connect(show_quit_message)
+        self.actionSearch_on_Web.triggered.connect(show_web_search_dialog)
+        self.actionPlay.triggered.connect(play)
+        self.actionPause.triggered.connect(pause)
+        self.actionStop.triggered.connect(stop)
